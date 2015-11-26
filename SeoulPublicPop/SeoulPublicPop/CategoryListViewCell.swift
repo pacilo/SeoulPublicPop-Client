bd68899e03e719a7590a85c49f562426fe31e720
pacilo <pacilo93@gmail.com>
2015-11-26 11:14:39 +0900

    Complete Category List (EntryPoint)

---
+++ SeoulPublicPop/SeoulPublicPop/CategoryListViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryListViewCell.swift
+//  SeoulPublicPop
+//
+//  Created by pacilo on 2015. 11. 26..
+//  Copyright © 2015년 Pacilo's Lab. All rights reserved.
+//
+
+import UIKit
+
+class CategoryListViewCell: UICollectionViewCell {
+    
+}
---
+++ SeoulPublicPop/SeoulPublicPop/CategoryModel.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryListModel.swift
+//  SeoulPublicPop
+//
+//  Created by pacilo on 2015. 11. 26..
+//  Copyright © 2015년 Pacilo's Lab. All rights reserved.
+//
+
+import UIKit
+
+class CategoryListModel {
+    
+    var title: String
+    var backgroundImage: UIImage
+    
+    init(title: String, backgroundImage: UIImage) {
+        self.title = title
+        self.backgroundImage = backgroundImage
+    }
+    
+    convenience init(dictionary: NSDictionary) {
+        let title = dictionary["Title"] as? String
+        let backgroundName = dictionary["Background"] as? String
+        let backgroundImage = UIImage(named: backgroundName!)
+        self.init(title: title!, backgroundImage: backgroundImage!.decompressedImage)
+    }
+}
+
+extension UIImage {
+    
+    var decompressedImage: UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, true, 0)
+        drawAtPoint(CGPointZero)
+        let decompressedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return decompressedImage
+    }
+    
+}
