0bc43d66bafaea6cd08e866fbc99d3a48e654c09
pacilo <pacilo93@gmail.com>
2015-12-17 09:37:50 +0900

    Add LocalSearch Based

---
+++ SeoulPublicPop/SeoulPublicPop/ChunkOfRequestInfo.swift
@@ -0,0 +1,22 @@
+//
+//  ChunkOfRequestInfo.swift
+//  SeoulPublicPop
+//
+//  Created by pacilo on 2015. 12. 17..
+//  Copyright © 2015년 Pacilo's Lab. All rights reserved.
+//
+
+import Foundation
+
+struct RequestChunk {
+    var categoryType: String!
+    var localeName: String?
+    // 여긴 GPS 좌표정보 + 카츄가 정의해야함.
+}
+
+class ChunkOfRequestInfo {
+    var searchWayType: String!
+    var requestChunk: RequestChunk!
+}
+
+// 미완성 코드임 ㅎ.ㅎ...
